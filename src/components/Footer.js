81598020f56072ecb0f399afb14da30c9724ad39
Orlando-Z <104393776+Orlando-Z@users.noreply.github.com>
2022-06-20 14:53:39 -0500

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,81 @@
+import { Container, Row, Col, } from 'reactstrap';
+import Map from '../app/assets/img/map.PNG';
+
+const Footer = () => {
+    return (
+        <footer>
+            
+            <Container className=' top-footer container-fluid'>
+                <Row className=''>
+                    
+                    <Col className='text-end'>
+                        <img src={Map} alt='map-location' className='map-image' />
+                    </Col>
+
+                    <Col className='text-start'>
+                        <h5>Proto Animal Shelter</h5>
+                            <p>123 Main St <br />
+                            Anywhere, USA 12345 <br /></p>
+                        <a role='button'
+                            className='btn btn-link'
+                            href='tel:+13125551234'>
+                            <i className='fa fa-phone' />1-312-555-1234
+                        </a>
+                        <br />
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='mailto:notreal@notreal.co'
+                        >
+                            <i className='fa fa-envelope-o' /> PortoAnimalShelter.com
+                        </a>
+                    </Col>
+                    
+                </Row>
+            </Container>
+
+            
+            
+
+            <Container className='sub-footer container-fluid'>
+                <Row>
+                    <Col className='text-start'>
+                        <div className='text-white mt-1'>&copy; 2022 Company, Inc</div>
+                    </Col>
+
+
+                    
+
+                    <Col className='text-end'>
+                        <a
+                            className='btn btn-social-icon btn-instagram'
+                            href='http://instagram.com/'
+                        >
+                            <i className='fa fa-instagram' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-facebook'
+                            href='http://www.facebook.com/'
+                        >
+                            <i className='fa fa-facebook' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-twitter'
+                            href='http://twitter.com/'
+                        >
+                            <i className='fa fa-twitter' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-google'
+                            href='http://youtube.com/'
+                        >
+                            <i className='fa fa-youtube' />
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer;
