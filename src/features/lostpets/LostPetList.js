87ab553db02c1c8d6b3abf9c88da4bb81fa3b909
Christina Rontell <clrontell@gmail.com>
2022-07-02 20:02:42 -0500

    attempt, useState to filter lost pets

--- src/features/lostpets/LostPetList.js
+++ src/features/lostpets/LostPetList.js
@@ -1,26 +1,47 @@
 import LostPetCard from "./LostPetCard";
-import { Row, Col } from "reactstrap";
-import { selectAllLostPets } from "./lostPetSlice";
+import { useState } from "react";
+import { Row, Col, Container, Button, ButtonGroup } from "reactstrap";
+import { selectAllLostPets, selectLostDogs, selectLostCats, selectLostOthers } from "./lostPetSlice";
 
 const LostPetList = () => {
-    const lostpets = selectAllLostPets();
-    console.log('lost pets:', lostpets)
+
+    const [filter, setFilter] = useState('all')
+
+    const handleFilter = (value) => {
+        setFilter(value);
+        
+        if (value === 'dog') { selectLostDogs() }
+        else if (value === 'cat') { selectLostCats() }
+        else if (value === 'other') { selectLostOthers() }
+        else if (value === 'all') { selectAllLostPets() }
+        
+    };
 
     return (
-        <Row>
-            {lostpets.map((lostpet) => {
-                return (
-                    <Col
-                        sm='6'
-                        md='4'
-                        lg='3'
-                        key={lostpet.id}
-                    >
-                        <LostPetCard lostpet={lostpet} />
-                    </Col>
-                );
-            })}
-        </Row>
+        <Container>
+            <Row>
+                <ButtonGroup>
+                    <Button onClick={() => handleFilter('dogs')}>Dogs</Button>
+                    <Button onClick={() => handleFilter('cats')}>Cats</Button>
+                    <Button onClick={() => handleFilter('other')}>Other</Button>
+                    <Button color="danger" onClick={() => handleFilter('all')}>Reset</Button>
+                </ButtonGroup>
+            </Row>
+            <Row>
+                {filter.map((lostpet) => {
+                    return (
+                        <Col
+                            sm='6'
+                            md='4'
+                            lg='3'
+                            key={lostpet.id}
+                        >
+                            <LostPetCard lostpet={lostpet} />
+                        </Col>
+                    );
+                })}
+            </Row>
+        </Container>
     );
 };
 export default LostPetList;
--- src/features/lostpets/lostPetSlice.js
+++ src/features/lostpets/lostPetSlice.js
@@ -2,4 +2,16 @@ import { LOSTPETS } from "../../app/data/LOSTPETS";
 
 export const selectAllLostPets = () => {
     return LOSTPETS;
+}; 
+
+export const selectLostDogs = () => {
+    return LOSTPETS.filter((object) => object.species === 'dog');
+};
+
+export const selectLostCats = () => {
+    return LOSTPETS.filter((object) => object.species === 'cat');
+};
+
+export const selectLostOthers = () => {
+    return LOSTPETS.filter((object) => object.species === 'other');
 };
